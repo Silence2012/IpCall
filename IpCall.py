0a0af0299f3b2bca7e6d32c7cd521b7e7836dada
Silence2012 <386488135@qq.com>
2014-07-04 09:51:18 +0800

    the name of the function is changed

--- IpCall.py
+++ IpCall.py
@@ -17,30 +17,27 @@ class IpCall(object):
         with open("ipUsed.txt","w") as fs:
             for i in range(len(self.listIpUsed)):
                 fs.writelines(self.listIpUsed[i]+'\n')
-            fs.flush()
+            #fs.flush()
 
-    def ipDirectGet(self,ipNum):
+    def ip_direct_req(self,ipNum):
         '''Get an ip from a file'''
         if ipNum > 0:
             while ipNum:
-
-
                 if self.listIp:
                     lista = self.listIp.pop(-1)
                     print lista
                     self.listIpUsed.append(lista)
                 else:
-
                     print "The ip pool is used up."
                     break
 
                 ipNum -= 1
             #print self.listIpUsed
             self.ip2file()
-        print "it does with success"
-        print self.listIpUsed
+        #print "ip_dirct_req does"
+        #print self.listIpUsed
 
-    def ipSpecifyGet(self,ipData):
+    def ip_specify_req(self,ipData):
         '''Get the specify ip function'''
 
         if ipData in self.listIpUsed:
@@ -56,7 +53,7 @@ class IpCall(object):
             return True
         else :
             print 'The input data is out of the ip range,redo it please!   '
-    def ipRelease(self,dataIp):
+    def ip_release(self,dataIp):
         '''This function is ursed to release ip'''
         if dataIp in self.listIpUsed:
             self.listIp.append(dataIp)
@@ -67,27 +64,29 @@ class IpCall(object):
             print 'This Ip is not in used , check it please'
 
 
-    def openfileFunc(self,filename):
+    def open_file_func(self,filename):
         '''open the ip file '''
         with open(filename) as fs:
-            self.listIp = fs.readlines()[0].split(',')
+            #print fs.readlines()
+            self.listIp = fs.readlines()
             if self.listIp[-1] == '':
                 self.listIp.pop()
+
             #print self.listIp
-    def ipInit(self):
+    def ip_init(self):
         '''Generate an ip range into a file'''
         #dataIp = raw_input('input the CIDR network (e.g.,192.168.1.0/24):  ')
-        dataIp = '192.168.1.0/24'
+        dataIp = u"192.168.1.0/24"
         net = ipaddress.ip_network(dataIp)
         with open("ip.txt",'wt') as f:
             for i in net :
                 f.writelines(str(i))
-                f.write(',')
+                f.write('\n')
 
 
     def __init__(self):
-        self.ipInit()
-        self.openfileFunc('ip.txt')
+        self.ip_init()
+        self.open_file_func('ip.txt')
 
         print '1.input an ip range       '
         print '2.input a specify ip      '
@@ -100,23 +99,24 @@ class IpCall(object):
                 break
             elif choice == 1:
                 num = input('input the number of ip you want to get:  ')
-                self.ipDirectGet(num)
+                self.ip_direct_req(num)
             elif choice == 2:
 
                 while True:
                     ipData = raw_input('input the specify ip(e.g.,192.168.1.x):  ')
                     if not ipData:break
-                    state = self.ipSpecifyGet(ipData)
+                    state = self.ip_specify_req(ipData)
                     if state:
                         break
             elif choice == 3:
                 dataIp = raw_input("Input the ip which you want to release :")
-                self.ipRelease(dataIp)
+                self.ip_release(dataIp)
             else:
                 print "choise the 1 , 2 or 3  !"
 
     if __name__ == '__main__':
         pass
+
 
 
 a = IpCall()
